48e214ec7facd0885e220f1660b7e9f718513c63
Dhanush M <mdhanushraju345@gmail.com>
2025-11-18 00:45:32 +0530

    pro

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,61 +1,61 @@
+import "./Project.css";
+import recipeImg from '../assets/recipe.png';
+import Portfolio from '../assets/portfolio.png';
+import weather from "../assets/weather.png";
 
-import "./Project.css"
-import recipeImg from '../assets/recipe.png'
-import Portfolio from '../assets/portfolio.png'
-import weather from "../assets/weather.png"
 export default function Project() {
+  const projects = [
+    {
+      title: "Recipe Finder",
+      img: recipeImg,
+      alt: "Screenshot of Recipe Finder app",
+      url: "https://mdhanushraju.github.io/Project-Food-recipe/"
+    },
+    {
+      title: "Portfolio",
+      img: Portfolio,
+      alt: "Screenshot of portfolio project",
+      url: "https://mdhanushraju.github.io/Portfolio_MDR/"
+    },
+    {
+      title: "Weather-checker",
+      img: weather,
+      alt: "Screenshot of weather checker app",
+      url: "https://weather-location-six.vercel.app/"
+    }
+  ];
+
   return (
     <>
-  <h1 className='text-center'>Projects</h1>
+      <h1 className='text-center'>Projects</h1>
       <div className="projects" id='project'>
         <div className="po">
-          <div className="po-container">
-            <div className="card-1">
-              <div className="card-title bg-dark-subtle text-center">
-                <h3>Recipe Finder</h3>
-              </div>
-              <div className="card-body body-1">
-                <img src={recipeImg} alt="" className='img' />
-              </div>
-              <div className="card-footer foot-1 ">
-                <button className='btn btn-success'>
-                  <a href="https://mdhanushraju.github.io/Project-Food-recipe/" target='_blank' className='a-1' style={{textDecoration:"none",color:"white"}}>view</a>
-                </button>
-              </div>
-            </div>
-          </div>
-         <div className="po-container">
-            <div className="card-1">
-              <div className="card-title bg-dark-subtle text-center">
-                <h3>Portfolio</h3>
-              </div>
-              <div className="card-body body-1">
-                <img src={Portfolio} alt="Portfolio screenshot" className='img' />
-              </div>
-              <div className="card-footer foot-1 ">
-                <button className='btn btn-success'>
-                  <a href="https://mdhanushraju.github.io/Portfolio_MDR/" target='_blank' className='a-1' style={{textDecoration:"none",color:"white"}}>view</a>
-                </button>
+          {projects.map(({ title, img, alt, url }, index) => (
+            <div className="po-container" key={index}>
+              <div className="card-1" aria-label={`${title} project card`}>
+                <div className="card-title bg-dark-subtle text-center">
+                  <h3>{title}</h3>
+                </div>
+                <div className="card-body body-1">
+                  <img src={img} alt={alt} className='img' />
+                </div>
+                <div className="card-footer foot-1 ">
+                  <a
+                    href={url}
+                    target='_blank'
+                    rel="noopener noreferrer"
+                    className='btn btn-success'
+                    style={{ textDecoration: 'none', color: 'white' }}
+                    aria-label={`View ${title} project`}
+                  >
+                    View
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="po-container">
-            <div className="card-1">
-              <div className="card-title bg-dark-subtle text-center">
-                <h3>Weather-checker</h3>
-              </div>
-              <div className="card-body body-1">
-                <img src={weather} alt="Weather app screenshot" className='img' />
-              </div>
-              <div className="card-footer foot-1 ">
-                <button className='btn btn-success'>
-                  <a href="https://weather-location-six.vercel.app/" target='_blank' className='a-1' style={{textDecoration:"none",color:"white"}}>view</a>
-                </button>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
-  )
+  );
 }
