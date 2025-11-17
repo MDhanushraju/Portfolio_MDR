5fcb75ebfc79186ffca04727314976223781274e
Dhanush M <mdhanushraju345@gmail.com>
2025-11-18 00:14:33 +0530

    dep

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
     import 'bootstrap/dist/css/bootstrap.min.css';
     import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App'
+import { HashRouter } from 'react-router-dom';
 
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <App/>
+    <HashRouter>
+ <App/>
+    </HashRouter>
+  
   </StrictMode>,
 )
